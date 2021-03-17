1b983cd236a988a2b6e1158f949f5138e8ac8c8d
C7C4FF <80257041+C7C4FF@users.noreply.github.com>
2021-03-17 22:21:35 +0900

    03. 11. 과제

---
+++ 01/app.js
@@ -0,0 +1,40 @@
+console.log('app.js');
+
+// var image = {
+//     width: 100,
+//     height: 100,
+//     name: '1.png',
+// };
+
+// var image1 = [1, 2,3,4,5,6, 'a', {}];
+// image1[0] = 'a';
+
+// var data = null;
+// var data1 = '';
+// var data2;
+
+// console.log(data, data1, data2);
+// console.log(image, image1);
+
+// var count = 1;
+// var number = '2';
+
+// console.log(1 + 2);
+// console.log('a' + 'b');
+// console.log(1 + '4');
+
+// console.log(count + number)
+// == 동등 | === 일치
+// console.log(0 === '', 0 == '');
+
+// var isHero = false;
+
+// if (!isHero){
+//     console.log('Hero')
+// }
+
+// console.log(typeof 1);
+// console.log(typeof []);
+// console.log(typeof {});
+// console.log(typeof '1');
+
